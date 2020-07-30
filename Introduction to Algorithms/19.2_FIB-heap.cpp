d31198c00e8622674b4d22aa95098264510bb791
wxy <1165205374@qq.com>
2020-07-30 14:09:20 +0800

    19.2_斐波纳契堆（插入、取小）

---
+++ Introduction to Algorithms/19.2_FIB-heap.cpp
@@ -0,0 +1,219 @@
+// 斐波纳契堆
+
+#include <iostream>
+#include <vector>
+#include <math.h>
+using namespace std;
+
+class Node
+{
+public:
+    int key;
+    Node *right = NULL;
+    Node *left = NULL;
+    Node *father = NULL;
+    Node *child = NULL;
+    int degree = 0;
+    bool mark = false;
+    Node(int k)
+    {
+        key = k;
+    }
+};
+
+class FIB_Heap
+{
+private:
+    Node *min = NULL;
+    int num = 0;
+
+public:
+    void FIB_insert(int k)
+    {
+        Node *x = new Node(k);
+        if (min == NULL)
+        {
+            min = x;
+            x->left = x;
+            x->right = x;
+        }
+        else
+        {
+            x->left = min;
+            x->right = min->right;
+            x->left->right = x;
+            x->right->left = x;
+            if (k < min->key)
+            {
+                min = x;
+            }
+        }
+        num++;
+    }
+    void FIB_union(FIB_Heap heap)
+    {
+        if (heap.min == NULL)
+        {
+            return;
+        }
+        else if (this->min == NULL)
+        {
+            min = heap.min;
+            num = heap.num;
+        }
+        else
+        {
+            union_heap(heap.min, this->min);
+            if (heap.min->key < this->min->key)
+            {
+                this->min = heap.min;
+            }
+            this->num += heap.num;
+        }
+    }
+    void union_heap(Node *a, Node *b)
+    {
+        Node *c = a->right;
+        Node *d = b->right;
+        c->left = b;
+        d->left = a;
+        a->right = d;
+        b->right = c;
+    }
+    Node *FBI_extract_min()
+    {
+        Node *z = min;
+        if (z != NULL)
+        {
+            if (z->degree != 0)
+            {
+                union_heap(this->min, z->child);
+                Node *temp = min->right;
+                while (temp->left != z->child)
+                {
+                    temp->father = NULL;
+                    temp = temp->right;
+                }
+            }
+            if (z->right == z)
+            {
+                min = NULL;
+            }
+            else
+            {
+                min->right->left = min->left;
+                min->left->right = min->right;
+                min = min->right;
+                consolidate();
+            }
+            num--;
+        }
+        return z;
+    }
+    void consolidate()
+    {
+        vector<Node *> A(floor(log2(num)) + 2, NULL);
+        Node *w = min;
+        for (int i = 0; i < num; i++)
+        {
+            Node *x = w;
+            int d = w->degree;
+            while (A[d] != NULL && A[d] != x)
+            {
+                Node *y = A[d];
+                if (y->key < x->key)
+                {
+                    Node *a = y->left;
+                    Node *b = x->right;
+
+                    y->left->right = y->right;
+                    y->right->left = y->left;
+                    x->left->right = x->right;
+                    x->right->left = x->left;
+
+                    x->left = a;
+                    x->right = a->right;
+                    x->left->right = x;
+                    x->right->left = x;
+
+                    y->right = b;
+                    y->left = b->left;
+                    y->left->right = y;
+                    y->right->left = y;
+
+                    FIB_link(x, y);
+                    x = y;
+                }
+                else
+                {
+                    FIB_link(y, x);
+                }
+                A[d] = NULL;
+                d++;
+            }
+            w = x->right;
+            A[d] = x;
+        }
+        min = NULL;
+        for (int i = 0; i < A.size(); i++)
+        {
+            if (A[i] != NULL)
+            {
+                if (min == NULL)
+                {
+                    min = A[i];
+                }
+                else
+                {
+                    if (A[i]->key < min->key)
+                    {
+                        min = A[i];
+                    }
+                }
+            }
+        }
+    }
+    void FIB_link(Node *y, Node *x)
+    {
+        y->right->left = y->left;
+        y->left->right = y->right;
+        if (x->child == NULL)
+        {
+            x->child = y;
+            y->left = y;
+            y->right = y;
+        }
+        else
+        {
+            x->child->right->left = y;
+            y->right = x->child->right;
+            x->child->right = y;
+            y->left = x->child;
+        }
+        y->father = x;
+        x->degree++;
+        y->mark = false;
+    }
+};
+
+int main()
+{
+    FIB_Heap *heap = new FIB_Heap();
+    heap->FIB_insert(3);
+    heap->FIB_insert(7);
+    heap->FIB_insert(1);
+    heap->FIB_insert(2);
+    heap->FIB_insert(10);
+    heap->FIB_insert(4);
+    heap->FIB_insert(5);
+    heap->FIB_insert(18);
+    heap->FIB_insert(20);
+    heap->FIB_insert(14);
+    heap->FIB_insert(17);
+    heap->FIB_insert(0);
+    heap->FIB_insert(16);
+    heap->FIB_insert(21);
+    heap->consolidate();
+    heap->FBI_extract_min();
+    return 0;
+}
